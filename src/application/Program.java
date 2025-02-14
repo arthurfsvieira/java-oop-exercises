998e0ecbf5e1bf2b8b4688b8b37b6739cdd5a45e
Arthur Vieira <arthur.devbr@gmail.com>
2025-02-14 12:59:31 +0000

    constructor exercise

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import util.CurrencyConverter;
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,15 +8,6 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("What's the dollar price? ");
-        double dollarPrice = sc.nextDouble();
-        System.out.print("How many dollars will be bought? ");
-        double amount = sc.nextDouble();
-        double result = CurrencyConverter.dollarToReal(amount, dollarPrice);
-
-        System.out.printf("Amount to be paid in reais: R$ %.2f%n", result);
-
         sc.close();
     }
 }
-//Aula 74 - Construtores
